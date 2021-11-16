f6997e964e47e934167ff9d78053665c75c08fd0
Pedro <pedro.rodrigues@melix.org>
2021-11-16 12:17:05 +0100

    ...

--- Ex3-Microphones/inference-script.py
+++ Ex3-Microphones/inference-script.py
@@ -3,12 +3,12 @@ from collections import OrderedDict
 import numpy as np
 
 LIST_NEXTRA = list(np.unique(np.logspace(0, 3, 20, dtype=int)))
-for noise in [0.00, 0.05]:
+for sigma in [0.00, 0.05]:
     for globalcoord in ['u', 'v']:
         for nextra in LIST_NEXTRA:
             parameters = OrderedDict()
             parameters["nextra"] = nextra
-            parameters["noise"] = noise
+            parameters["sigma"] = sigma
             parameters["globalcoord"] = globalcoord
             command = "python inference.py"
             for key in parameters.keys():
