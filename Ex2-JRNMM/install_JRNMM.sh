d1994c70c7450f680e9b16e1656e403edad0ee02
tomMoral <thomas.moreau.2010@gmail.com>
2021-11-26 15:16:57 +0100

    FIX install rpy2 on ubuntu

--- Ex2-JRNMM/install_JRNMM.sh
+++ Ex2-JRNMM/install_JRNMM.sh
@@ -1,2 +1,3 @@
-conda install -c r rpy2 r-devtools r-bh
+conda install -c r r-devtools r-bh
+pip install rpy2
 Rscript -e "devtools::install_git('https://github.com/massimilianotamborrino/sdbmpABC.git')"
